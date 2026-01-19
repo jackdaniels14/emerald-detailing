92433baaee6f847ce90acc2e719bd99d85d1be68
jackdaniels14 <emeralddetailer@gmail.com>
2026-01-19 03:12:37 -0800

    Add basePath for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/emerald-detailing',
+  assetPrefix: '/emerald-detailing/',
   images: {
     unoptimized: true,
   },
